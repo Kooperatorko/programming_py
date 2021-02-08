16149372fca64b72c110651c8086155342b22598
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-08 22:49:09 +0300

    Update chetnost.py

--- chetnost.py
+++ chetnost.py
@@ -3,10 +3,12 @@ def func(res):
     x = int(input())
     print('Введите последнее число: ', end='')
     y = int(input())
-    while x < y:
+    while x <= y:
         if x % 2 == 0:
             res += x
             x += 2
+        else:
+            x += 1
     return(res)
 
-print('Сумма четных = ', func(0))
+print('Сумма четных = ', func(res = 0))
